2872d2124a7f3e67c128cd35a52c0c92e1247e4d
Matthew McCormack <mmccormack1182@gmail.com>
2018-08-30 23:16:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+require "pry"
+katz_deli=[]
+
+def line(array)
+  line = "The current line is: "
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    array.collect {|x| line +=  "#{array.index(x)+1}. #{x}"}
+    puts line
+  end
+end
+
+def take_a_number(array,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+end
